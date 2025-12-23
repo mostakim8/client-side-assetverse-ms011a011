2bd33868f0a65870be70e1083a2baf7a3d1fd521
mostakim8 <allmostakim13@gmail.com>
2025-12-23 23:36:49 +0600

    env file edit

--- src/hooks/UseRole.jsx
+++ src/hooks/UseRole.jsx
@@ -8,7 +8,9 @@ const UseRole = () => {
         queryKey: [user?.email, 'role'],
         enabled: !loading && !!user?.email,
         queryFn: async () => {
-            const res = await axios.get(`${import.meta.env.VITE_API_URL}/users/role/${user?.email}`); 
+            // const res = await axios.get(`${import.meta.env.VITE_API_URL}/users/role/${user?.email}`); 
+
+            const res = await axios.get(`https://assetverse-server-side-ms011a011.vercel.app/users/role/${user?.email}`);
             console.log("Current User Role:", res.data?.role); 
             return res.data?.role;
         }
